741aeecd5a8d3d7d35ffe2f76fdf185368789765
KOVURUMAHENDRA79 <mahendrakovuru2489@gmail.com>
2025-04-17 20:33:31 +0530

    Update fibinocci series.py

--- fibinocci series.py
+++ fibinocci series.py
@@ -5,10 +5,7 @@ elif n == 1:
     print("Fibonacci series: 0")
 else:
     f = [0, 1]
-    print("Fibonacci series:")
-    print(f[0], f[1], end=" ")
     for i in range(2, n):
-        num = f[-1] + f[-2]
-        f.append(num)
-        print(num, end=" ")  
-    print()
+        f.append(f[-1] + f[-2])
+    print("Fibonacci series:")
+    print(" ".join(map(str, f)))
